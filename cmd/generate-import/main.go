bad0ed2798b1f8fd0869d82a5ee7767129b2ed80
Yuce Tekol <yucetekol@gmail.com>
2018-03-28 06:44:03 +0300

    update before deleing generate-import

--- cmd/generate-import/main.go
+++ cmd/generate-import/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"math/rand"
@@ -10,21 +10,66 @@ import (
 	"strings"
 )
 
+type Generator interface {
+	nextBit() (int64, int64, bool)
+}
+
+type RandomGenerator struct {
+	rnd      *rand.Rand
+	maxRowID int64
+	maxColID int64
+	remBits  int
+}
+
+func NewRandomGenerator(args []string) (*RandomGenerator, error) {
+	if len(args) != 4 {
+		return nil, errors.New("Required params: RANDOM_SEED MAX_ROW_ID MAX_COL_ID BIT_COUNT")
+	}
+	seed := int64(parseInt(args[0]))
+	maxRowID := int64(parseInt(args[1]))
+	maxColID := int64(parseInt(args[2]))
+	bitCount := parseInt(args[3])
+	return &RandomGenerator{
+		rnd:      rand.New(rand.NewSource(seed)),
+		maxRowID: maxRowID,
+		maxColID: maxColID,
+		remBits:  bitCount,
+	}, nil
+}
+
+func (g *RandomGenerator) nextBit() (int64, int64, bool) {
+	if g.remBits <= 0 {
+		return 0, 0, false
+	}
+	g.remBits -= 1
+	return g.rnd.Int63n(g.maxRowID), g.rnd.Int63n(g.maxColID), true
+}
+
 func main() {
-	if len(os.Args) != 6 {
-		fmt.Printf("Usage: %s RANDOM_SEED MAX_ROW_ID MAX_COLUMN_ID BIT_COUNT PATH.csv\n", os.Args[0])
-		fmt.Println("Pass - as the PATH to output to stdout")
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: %s GEN_STRATEGY [STRATEGY_PARAMS]\n", os.Args[0])
 		os.Exit(1)
 	}
 
-	randomSeed := int64(parseInt(os.Args[1]))
-	maxRowID := int64(parseInt(os.Args[2]))
-	maxColID := int64(parseInt(os.Args[3]))
-	bitCount := parseInt(os.Args[4])
-	outputFile := os.Args[5]
+	var gen Generator
+	var err error
+	strategy := os.Args[1]
+
+	switch strategy {
+	case "random":
+		gen, err = NewRandomGenerator(os.Args[2:])
+	default:
+		err = fmt.Errorf("Unknown strategy: %s", strategy)
+	}
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	outputFile := "-"
 
 	var f *os.File
-	var err error
 
 	if outputFile == "-" {
 		f = os.Stdout
@@ -36,12 +81,14 @@ func main() {
 		defer f.Close()
 	}
 
-	randGen := rand.New(rand.NewSource(randomSeed))
-	w := bufio.NewWriter(f)
+	// w := bufio.NewWriter(f)
+	w := f
 
-	for i := 0; i < bitCount; i++ {
-		rid := randGen.Int63n(maxRowID)
-		cid := randGen.Int63n(maxColID)
+	for {
+		rid, cid, ok := gen.nextBit()
+		if !ok {
+			break
+		}
 		_, err := w.WriteString(fmt.Sprintf("%d,%d\n", rid, cid))
 		if err != nil {
 			log.Fatal(err)
